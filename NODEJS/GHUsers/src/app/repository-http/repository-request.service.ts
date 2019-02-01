69a86cc76545b4038d123b84d16a4745aad3418b
JURU <jurassu10@gmail.com>
2019-02-01 15:05:23 +0200

    add imports in the repository request service

--- NODEJS/GHUsers/src/app/repository-http/repository-request.service.ts
+++ NODEJS/GHUsers/src/app/repository-http/repository-request.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {User} from '../user';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class RepositoryRequestService {
 
-  constructor() { }
+  user:User;
+  constructor(private http:HttpClient) {
+    this.repository=new Repository('','');
+  }
 }
