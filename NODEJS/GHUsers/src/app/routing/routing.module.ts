8861a4ffab48faeaae20667392013356409ae32d
JURU <jurassu10@gmail.com>
2019-02-01 15:06:28 +0200

    add the routing system

--- NODEJS/GHUsers/src/app/routing/routing.module.ts
+++ NODEJS/GHUsers/src/app/routing/routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {RouterModule,Routes} from '@angular/router';
+
+import {UserDetailsComponent} from '../user-details.component';
+
+import { UserComponent } from '../user/user.component';
+import { AboutComponent } from '../about/about.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+
+const routes:Routes=[
+  {path:"users",component:UserComponent},
+  {path:"about",component:AboutComponent},
+  {path:"",redirectTo:"/users",pathMatch:"full"},
+  {path:'users/:id',component:UserDetailsComponent},
+  {path:'**',component:NotFoundComponent}
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule],
 })
 export class RoutingModule { }
