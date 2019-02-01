b5e3501ebc6731af7035ced4cb2e0fe8cdebd001
JURU <jurassu10@gmail.com>
2019-02-01 10:24:58 +0200

    add all services and routing folders

--- NODEJS/GHUsers/src/app/app.module.ts
+++ NODEJS/GHUsers/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AboutComponent,
+    NotFoundComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
