0b8b9f47b29df5b5baf428c13a2fd827c1d4bb31
JURU <jurassu10@gmail.com>
2019-02-01 15:08:15 +0200

    add the importing in the user component

--- NODEJS/GHUsers/src/app/user/user.component.ts
+++ NODEJS/GHUsers/src/app/user/user.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import {User} from '../user';
+import {Users} from '../users';
+import {HttpClient} from '@angular/common/http';
+import {Repository} from '../repository';
+import {Router} from '@angular/router';
+import {UserService} from '../users/user.service';
+import {AlertsService} from '../alert-service/alerts.service';
+import {RepositoryRequestService} from '../repository-http/repository-request.service';
+
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
