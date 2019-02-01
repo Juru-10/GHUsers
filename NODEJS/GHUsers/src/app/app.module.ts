b8886f8ba747302fa6f86b16836141eb6c8d17a8
JURU <jurassu10@gmail.com>
2019-02-01 15:26:21 +0200

    add the piepe

--- NODEJS/GHUsers/src/app/app.module.ts
+++ NODEJS/GHUsers/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { StrikethroughDirective } from './strikethrough.directive';
     AboutComponent,
     NotFoundComponent,
     UserDetailsComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
